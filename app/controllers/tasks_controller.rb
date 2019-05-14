56568fead5dc32e17eb5b596aaeaed0362c41d7d
Joel Velis <joelvelis94@gmail.com>
2019-05-14 15:04:01 +0200

    added the ability to add new tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,21 @@ class TasksController < ApplicationController
         @tasks = Task.all
     end
 
-    def show 
+    def show
         @task = Task.find(params[:id])
+    end
+
+    def new
+        @task = Task.new
+    end
+    
+    def create
+        task = Task.new(strong_task_params)
+        task.save
+        redirect_to '/tasks'
+    end
+
+    def strong_task_params
+        params.require(:task).permit(:title, :details)
     end
 end
